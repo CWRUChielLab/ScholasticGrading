298e893fb2afe58680c7d2e9759999c3a22711c4
Jeffrey Gill <jpg18@case.edu>
2013-02-02 22:56:56 -0500

    Form and table for scholasticgrading_assignment

--- SpecialGrades.php
+++ SpecialGrades.php
@@ -30,13 +30,24 @@ class SpecialGrades extends SpecialPage {
 
         # Process requests
         $action = $par ? $par : $request->getVal('action', $par);
+        $form   = $par ? $par : $request->getVal('form', $par);
         switch ( $action ) {
         case 'submit':
-            $this->doSubmit();
+            switch ( $form ) {
+            case 'test':
+                $this->doSubmit();
+                break;
+            case 'assignment':
+                $this->doAssignmentSubmit();
+                break;
+            }
         }
 
-        $this->showForm();
-        $this->showList();
+        //$this->showForm();
+        //$this->showList();
+
+        $this->showAssignmentForm();
+        $this->showAssignments();
     }
 
 
@@ -64,7 +75,7 @@ class SpecialGrades extends SpecialPage {
         $out .= Xml::fieldset( "fieldset",
             Html::rawElement('form', array('method' => 'post',
                 'action' => $this->getTitle()->getLocalUrl(
-                    array('action' => 'submit', 'param' => 'form-param'))),
+                    array('action' => 'submit', 'form' => 'test', 'param' => 'form-param'))),
                  Html::rawElement('table', null,
                     Html::rawElement('tr', null,
                         Html::rawElement('td', null, Xml::label('label-label', 'input-id')) .
@@ -93,6 +104,86 @@ class SpecialGrades extends SpecialPage {
             $out .= Html::rawElement('tr', null,
                 Html::element('td', null, $row->test_id) .
                 Html::element('td', null, $row->foo)
+            ) . "\n";
+        }
+        $out .= Html::closeElement('table');
+        $this->getOutput()->addHTML($out);
+    }
+
+
+    public function doAssignmentSubmit () {
+        $request = $this->getRequest();
+        $assignmentTitle = $request->getVal('assignment-title');
+        $assignmentValue = $request->getVal('assignment-value');
+        $assignmentEnabled = $request->getCheck('assignment-enabled') ? 1 : 0;
+
+        $dbw = wfGetDB( DB_MASTER );
+        $dbw->insert('scholasticgrading_assignment', array(
+            'sga_title' => $assignmentTitle,
+            'sga_value' => $assignmentValue,
+            'sga_enabled' => $assignmentEnabled
+        ));
+
+        if ( $dbw->affectedRows() === 0 ) {
+            $this->getOutput()->addWikiText('Database unchanged.');
+        } else {
+            $this->getOutput()->addWikiText('\'\'\'"' . $assignmentTitle . '" added!\'\'\'');
+
+            $log = new LogPage('grades', false);
+            $log->addEntry('add', $this->getTitle(), $assignmentTitle);
+        }
+    }
+
+
+    public function showAssignmentForm () {
+        # Add entries to the database table test
+        $out = '';
+        $out .= Xml::fieldset( "Create a new assignment",
+            Html::rawElement('form', array('method' => 'post',
+                'action' => $this->getTitle()->getLocalUrl(
+                    array('action' => 'submit', 'form' => 'assignment'))),
+                 Html::rawElement('table', null,
+                    Html::rawElement('tr', null,
+                        Html::rawElement('td', null, Xml::label('Title:', 'assignment-title')) .
+                        Html::rawElement('td', null, Xml::input('assignment-title', 20, '', array('id' => 'assignment-title')))
+                    ) .
+                    Html::rawElement('tr', null,
+                        Html::rawElement('td', null, Xml::label('Point value:', 'assignment-value')) .
+                        Html::rawElement('td', null, Xml::input('assignment-value', 20, '0', array('id' => 'assignment-value')))
+                    ) .
+                    Html::rawElement('tr', null,
+                        Html::rawElement('td', null, Xml::label('Enabled:', 'assignment-enabled')) .
+                        Html::rawElement('td', null, Xml::check('assignment-enabled', true, array('id' => 'assignment-enabled')))
+                    )
+                ) .
+                Xml::submitButton('Create assignment')
+            )
+        );
+        $this->getOutput()->addHTML($out);
+    }
+
+
+    public function showAssignments () {
+        # List all assignments
+        $dbr = wfGetDB(DB_SLAVE);
+        $res = $dbr->select('scholasticgrading_assignment', '*');
+
+        $out = '';
+        $out .= Html::openElement('table', array('class' => 'wikitable')) . "\n";
+        $out .= Html::rawElement('tr', null,
+            Html::element('th', null, 'id') .
+            Html::element('th', null, 'title') .
+            Html::element('th', null, 'value') .
+            Html::element('th', null, 'enabled') .
+            Html::element('th', null, 'date')
+        ) . "\n";
+        foreach ( $res as $row ) {
+            $out .= Html::rawElement('tr', null,
+                Html::element('td', null, $row->sga_id) .
+                Html::element('td', null, $row->sga_title) .
+                Html::element('td', null, $row->sga_value) .
+                Html::element('td', null, $row->sga_enabled) .
+                Html::element('td', null, $row->sga_date)
             ) . "\n";
         }
         $out .= Html::closeElement('table');
